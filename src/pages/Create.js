8362c3aab3a7e980a28772cc7045d9fffc592652
Manoj Silag <silagmanoj@yahoo.in>
2021-03-19 19:26:37 +0530

    Add files via upload

---
+++ src/pages/Create.js
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import Container from "@material-ui/core/Container";
+import KeyboardArrowRightIcon from "@material-ui/icons/KeyboardArrowRight";
+import { makeStyles } from "@material-ui/core";
+import TextField from "@material-ui/core/TextField";
+
+const useStyles = makeStyles({
+  field: {
+    marginTop: 20,
+    marginBottom: 20,
+    display: "block",
+  },
+});
+
+export default function Create() {
+  const classes = useStyles();
+
+  const [title, setTitle] = useState("");
+  const [details, setDetails] = useState("");
+  const [titleError, setTitleError] = useState(false);
+  const [detailsError, setDetailsError] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTitleError(false);
+    setDetailsError(false);
+    if(title === "") setTitleError(true)
+    if(details === "") setDetailsError(true)
+    if (title && details) console.log(title, details);
+  };
+
+  return (
+    <Container size="sm">
+      <Typography
+        variant="h6"
+        color="textSecondary"
+        component="h2"
+        gutterBottom
+      >
+        Create a New Note
+      </Typography>
+
+      <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+        <TextField
+          onChange={(e) => setTitle(e.target.value)}
+          className={classes.field}
+          id="outlined-basic"
+          label="Note title"
+          variant="outlined"
+          color="secondary"
+          fullWidth
+          required
+          error={titleError}
+        />
+        <TextField
+          onChange={(e) => setDetails(e.target.value)}
+          className={classes.field}
+          id="outlined-basic"
+          label="Details"
+          variant="outlined"
+          color="secondary"
+          fullWidth
+          required
+          multiline
+          rows={4}
+          error={detailsError}
+        />
+        <Button
+          type="submit"
+          color="secondary"
+          variant="contained"
+          endIcon={<KeyboardArrowRightIcon />}
+        >
+          Submit
+        </Button>
+      </form>
+    </Container>
+  );
+}
